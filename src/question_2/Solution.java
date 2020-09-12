0c8ec2c4898c58d9e8190f328f13566b0a7b23ad
Achyut <achyut.terminator@gmail.com>
2020-09-12 08:29:17 +0530

    commit

--- src/question_2/Solution.java
+++ src/question_2/Solution.java
@@ -1,3 +1,11 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input
+string is valid. An input string is valid if –
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+*/
+
+
 package question_2;
 import java.util.*;
 
