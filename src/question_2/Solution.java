fa137e3e08c8144ef807147f064fae819217f51f
Achyut <achyut.terminator@gmail.com>
2020-09-12 08:26:21 +0530

    commit

--- src/question_2/Solution.java
+++ src/question_2/Solution.java
@@ -1,5 +1,57 @@
 package question_2;
+import java.util.*;
 
 public class Solution {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of Test cases: ");
+		int n = Integer.parseInt(sc.next());
+		while(n-->0) {
+			System.out.println("Enter String of brackets: ");
+			String []s = sc.next().split("");
+			Stack<String> stack = new Stack<String>();
+			boolean flag = true;
+			for(int i=0; i<s.length; i++) {
+				if(s[i].equals("(") || s[i].equals("{") || s[i].equals("[")) {
+					stack.push(s[i]);
+				}
+				else{
+					if(s[i].equals(")")) {
+						if(stack.peek().equals("(")){
+							stack.pop();
+						}
+						else {
+							flag = false;
+							break;
+						}
+					}
+					else if(s[i].equals("}")) {
+						if(stack.peek().equals("{")){
+							stack.pop();
+						}
+						else {
+							flag = false;
+							break;
+						}
+					}
+					else if(s[i].equals("]")) {
+						if(stack.peek().equals("[")){
+							stack.pop();
+						}
+						else {
+							flag = false;
+							break;
+						}
+					}	
+				
+				}
+			}
+			
+			if(flag && stack.size() == 0)
+				System.out.println("True");
+			else 
+				System.out.println("False");
+		}
+		sc.close();
+	}
 }
